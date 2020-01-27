e35872ba85a68b9937972c3a832fecc504e2bc38
Romit Chaudhari <romitchaudhari@romits-mbp.attlocal.net>
2020-01-27 17:53:30 -0500

    populated database w items

--- bamazon.sql
+++ bamazon.sql
@@ -9,3 +9,17 @@ CREATE TABLE products (
 	stock_quantity INTEGER(11) NOT NULL,
 	PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES  ('Cup Ramen Noodle', 'Grocery', 5.00, 50),
+		('BAPE zip-up hoodie', 'Clothing', 250.00, 1),
+		('GameCube Spice-Orange Japan edition', 'Electronics', 399.99, 1),
+		('Stone Island Puffer Coat', 'Clothing', 755.00, 1),
+		('SSJ4 Goku Wax-Resin figurine', 'Collectibles', 329.99, 1),
+		('Frosted Flakes', 'Grocery', 3.99, 1),
+		('Playstation 4 White Destiny 2 edition', 'Electronics', 499.99, 1),
+		('Doritos Spicy Nachos party size', 'Grocery', 6.59, 2),
+		('Nike-tech fleece jogger sweatpants', 'Clothing', 59.99, 1),
+		('Sasuke Rinnegan Wax-Resin figurine', 'Collectibles', 289.99, 1),
+		('Apple Airpods V2 w/charging case', 'Electronics', 199.99, 1),
+		('Nesquik strawberry milk', 'Grocery', 29.99, 30);
